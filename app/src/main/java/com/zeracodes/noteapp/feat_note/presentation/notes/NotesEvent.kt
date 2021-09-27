d3d406b2fe75947fffef39dafbffbc00739e6d58
Luan Pinheiro <luan.pinheiro@popcode.com.br>
2021-09-27 18:12:40 -0300

    Criação da NoteScreen e implementação da regra de negócio desta.

--- app/src/main/java/com/zeracodes/noteapp/feat_note/presentation/notes/NotesEvent.kt
+++ app/src/main/java/com/zeracodes/noteapp/feat_note/presentation/notes/NotesEvent.kt
@@ -1,4 +1,4 @@
-package com.zeracodes.noteapp.feat_note.presentation.notes.components
+package com.zeracodes.noteapp.feat_note.presentation.notes
 
 import com.zeracodes.noteapp.feat_note.domain.model.Note
 import com.zeracodes.noteapp.feat_note.domain.util.NoteOrder
